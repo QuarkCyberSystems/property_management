2636cde93550cdfc9fab4faa1e0812d88f975bcc
Vivek <vivek@quarkcs.com>
2021-10-10 20:34:02 +0000

    new changes

--- property_management/property_management/doctype/tenancy_contract/tenancy_contract.py
+++ property_management/property_management/doctype/tenancy_contract/tenancy_contract.py
@@ -1,8 +1,20 @@
 # Copyright (c) 2021, QCS and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class TenancyContract(Document):
-	pass
+	def validate(self):
+		if self.status == "Active":
+			for item in self.rental_units_table:
+				ru = frappe.get_doc("Rental Units", item.rental_units)
+				ru.status = "Occupied"
+				ru.save()
+		if self.status == "Inactive":
+			for item in self.rental_units_table:
+				ru = frappe.get_doc("Rental Units", item.rental_units)
+				ru.status = "Occupied"
+				ru.save()
+
+
